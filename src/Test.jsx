18b8c6ce6f2d8b6e81f4d74988a31b6a45d17128
TiavinaMichael <ralainirinatiavina@gmail.com>
2021-09-02 20:33:20 +0300

    refactoring

--- src/Test.jsx
+++ src/Test.jsx
@@ -43,68 +43,73 @@ const template = {
   ]
 };
 
-// --------------------------- //
-// -------- copy files ------- //
-// --------------------------- //
-const userImageWithFiles = [];
-// get userImage layers
-const userImages = filterLayersByType(template.layers, "userImage");
-for (let i = 0; i < userImages.length; i++) {
-  userImageWithFiles[i] = [...files];
-}
-
-// --------------------------- //
-// ------------ pack --------- //
-// --------------------------- //
-// each pack should have a template => 4p x 1t x2l
-const newPackTemplates = [];
-
-new Array(4).fill(0).forEach((_, index) => {
-  const newTemplate = {
-    ...template
-  };
-  newPackTemplates.push(newTemplate);
-});
-
-let indexFile = 0;
-for (let i = 0; i < newPackTemplates.length; i++) {
-  const userImages = filterLayersByType(
-    newPackTemplates[i].layers,
-    "userImage"
-  );
-  for (let j = 0; j < userImages.length; j++) {
-    const file = files[indexFile];
-    userImages[j] = { ...userImages[j], file };
-    // userImages[j] = file;
-    indexFile++;
-    if (indexFile === files.length) {
-      // files length
-      indexFile = 0;
-    }
+const bindFilesToTemplates = (template) => {
+  // --------------------------- //
+  // -------- copy files ------- //
+  // --------------------------- //
+  const userImageWithFiles = [];
+  // get userImage layers
+  const userImages = filterLayersByType(template.layers, "userImage");
+  for (let i = 0; i < userImages.length; i++) {
+    userImageWithFiles[i] = [...files];
   }
 
-  const layersWithMask = [
-    ...newPackTemplates[i].layers.map((l) => {
-      if (!l.layers) {
-        if (l.type === "userText") {
-          return { ...l };
+  // --------------------------- //
+  // ------------ pack --------- //
+  // --------------------------- //
+  // each pack should have a template => 4p x 1t x2l
+  const newPackTemplates = [];
+
+  new Array(4).fill(0).forEach((_, index) => {
+    const newTemplate = {
+      ...template
+    };
+    newPackTemplates.push(newTemplate);
+  });
+
+  let indexFile = 0;
+  for (let i = 0; i < newPackTemplates.length; i++) {
+    const userImages = filterLayersByType(
+      newPackTemplates[i].layers,
+      "userImage"
+    );
+    for (let j = 0; j < userImages.length; j++) {
+      const file = files[indexFile];
+      userImages[j] = { ...userImages[j], file };
+      // userImages[j] = file;
+      indexFile++;
+      if (indexFile === files.length) {
+        // files length
+        indexFile = 0;
+      }
+    }
+
+    const layersWithMask = [
+      ...newPackTemplates[i].layers.map((l) => {
+        if (!l.layers) {
+          if (l.type === "userText") {
+            return { ...l };
+          }
+          return {
+            ...l,
+            ...userImages.find((u) => u.id === l.id)
+          };
         }
         return {
           ...l,
-          ...userImages.find((u) => u.id === l.id)
+          layers: userImages.filter((sb) =>
+            l.layers.find((u) => u.id === sb.id)
+          )
         };
-      }
-      return {
-        ...l,
-        layers: userImages.filter((sb) => l.layers.find((u) => u.id === sb.id))
-      };
-    })
-  ];
+      })
+    ];
 
-  newPackTemplates[i].layers = [...layersWithMask];
-}
+    newPackTemplates[i].layers = [...layersWithMask];
+    return newPackTemplates;
+  }
+};
 
-console.log("newPackTemplates", newPackTemplates);
+console.log("bindFilesToTemplates", bindFilesToTemplates(template));
 
 const Test = () => {
   return <div className="flexCenter">text</div>;

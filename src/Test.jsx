033f92f3ccdc0ec558ebd77c26f3538c095ee256
TiavinaMichael <ralainirinatiavina@gmail.com>
2021-09-03 09:25:32 +0300

    add json view

--- src/Test.jsx
+++ src/Test.jsx
@@ -1,6 +1,8 @@
 /** @jsxRuntime classic /
 /* @jsx jsx */
-import { jsx } from "@emotion/react";
+import { css, jsx } from "@emotion/react";
+import ReactJson from "react-json-view";
+
 import { filterLayersByType } from "./actions/templates";
 
 // ------------------------------- //
@@ -16,16 +18,6 @@ const files = [
 // ------------------------------- //
 // ------------ template --------- //
 // ------------------------------- //
-// const template = {
-//   name: "template1",
-//   layers: [
-//     { id: "m1", type: "mask", layers: [
-//       { id: "ui1", type: "userImage" },
-//       { id: "ui11", type: "userImage" },
-//     ] },
-//     { id: "ut1", type: "userText" },
-//   ]
-// };
 
 const template = {
   name: "template1",
@@ -34,30 +26,30 @@ const template = {
       id: "m1",
       type: "mask",
       layers: [
-        // { id: "ui1", type: "userImage" },
-        { id: "ui1", type: "userImage" }
+        { id: "ui1", type: "userImage" },
+        { id: "ui11", type: "userImage" },
+        { id: "ut1", type: "userText" },
+        { id: "i1", type: "image" }
       ]
     },
-    { id: "ut1", type: "userText" },
-    { id: "ui11", type: "userImage" }
+    { id: "ut2", type: "userText" },
+    { id: "i2", type: "image" }
+    // { id: "ui11", type: "userImage" }
   ]
 };
 
-const bindFilesToTemplates = (template) => {
-  // --------------------------- //
-  // -------- copy files ------- //
-  // --------------------------- //
-  const userImageWithFiles = [];
-  // get userImage layers
-  const userImages = filterLayersByType(template.layers, "userImage");
-  for (let i = 0; i < userImages.length; i++) {
-    userImageWithFiles[i] = [...files];
+const classes = {
+  reactJson: {
+    width: "100%",
+    padding: 20
   }
+};
 
-  // --------------------------- //
-  // ------------ pack --------- //
-  // --------------------------- //
-  // each pack should have a template => 4p x 1t x2l
+// --------------------------- //
+// ------------ pack --------- //
+// --------------------------- //
+// each pack should have a template => 4p x 1t x2l
+const bindFilesToTemplates = (template) => {
   const newPackTemplates = [];
 
   new Array(4).fill(0).forEach((_, index) => {
@@ -69,14 +61,13 @@ const bindFilesToTemplates = (template) => {
 
   let indexFile = 0;
   for (let i = 0; i < newPackTemplates.length; i++) {
-    const userImages = filterLayersByType(
+    const userImageLayers = filterLayersByType(
       newPackTemplates[i].layers,
       "userImage"
     );
-    for (let j = 0; j < userImages.length; j++) {
+    for (let j = 0; j < userImageLayers.length; j++) {
       const file = files[indexFile];
-      userImages[j] = { ...userImages[j], file };
-      // userImages[j] = file;
+      userImageLayers[j] = { ...userImageLayers[j], file };
       indexFile++;
       if (indexFile === files.length) {
         // files length
@@ -85,34 +76,55 @@ const bindFilesToTemplates = (template) => {
     }
 
     const layersWithMask = [
-      ...newPackTemplates[i].layers.map((l) => {
-        if (!l.layers) {
-          if (l.type === "userText") {
-            return { ...l };
+      ...newPackTemplates[i].layers.map((layer) => {
+        // layers level 1
+        if (!layer.layers) {
+          if (layer.type === "userText" || layer.type === "image") {
+            return { ...layer };
           }
+          // userImage layers level 1
           return {
-            ...l,
-            ...userImages.find((u) => u.id === l.id)
+            ...layer,
+            ...userImageLayers.find((u) => u.id === layer.id)
           };
         }
+
+        // layers level 2
         return {
-          ...l,
-          layers: userImages.filter((sb) =>
-            l.layers.find((u) => u.id === sb.id)
-          )
+          ...layer,
+          layers: [
+            // remove userImages not treated yet
+            ...layer.layers.filter((subLayer) => subLayer.type !== "userImage"),
+            // userImages with images
+            ...userImageLayers.filter((userImageLayer) =>
+              layer.layers.find((subLayer) => subLayer.id === userImageLayer.id)
+            )
+          ]
         };
       })
     ];
 
     newPackTemplates[i].layers = [...layersWithMask];
-    return newPackTemplates;
   }
+  return newPackTemplates;
 };
 
 console.log("bindFilesToTemplates", bindFilesToTemplates(template));
 
+// --------------------------- //
+// ------------ component --------- //
+// --------------------------- //
 const Test = () => {
-  return <div className="flexCenter">text</div>;
+  return (
+    <div className="flexCenter">
+      <ReactJson
+        src={bindFilesToTemplates(template)}
+        theme="pop"
+        displayDataTypes={false}
+        style={classes.reactJson}
+      />
+    </div>
+  );
 };
 
 export default Test;
